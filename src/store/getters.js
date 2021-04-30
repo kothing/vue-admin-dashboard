a5f9500da72d5f29ae923778c149e532fd6a8454
Kothing <39289309+kothing@users.noreply.github.com>
2021-04-30 19:14:58 +0800

    Update getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,6 @@
 
 // authorized lets you know if the token is true or not
 export default {
-  authorized: state => !!state.token,
+  authorized: state => !!state.authorized,
   authstatus: state => state.authStatus,
 }
